71bad9563519e74cfef628182b74eef89ee15936
Sebastian <sebastian.narloch82@gmail.com>
2021-05-01 18:09:31 +1000

    Finished exercise 1.7 (unicafe part 2)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,7 +6,10 @@ const Content = (props) => {
       <h2>Statistics</h2>
       <p>Good: {props.good}</p>
       <p>Neutral: {props.neutral}</p>
-      <p>Good: {props.bad}</p>
+      <p>Bad: {props.bad}</p>
+      <p>All: {props.all}</p>
+      <p>Average {props.average}</p>
+      <p>Positive {props.positive}%</p>
     </div>
   )
 }
@@ -16,29 +19,44 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const goodButtonHandler = (value) => {
     setGood(value += 1)
+    setTotal(total + 1)
+    setPositive((good + 1) / (total + 1) * 100)
+    setAverage(average + 1)
   }
   
   const neutralButtonHandler = (value) => {
     setNeutral(value += 1)
+    setTotal(total + 1)
+    setPositive((good / (total + 1)) * 100)
   }
   
   const badButtonHandler = (value) => {
     setBad(value += 1)
+    setTotal(total + 1)
+    setPositive((good / (total + 1)) * 100)
+    setAverage(average - 1)
   }
+
 
   return (
     <div className="App">
       <h1>{title}</h1>
       <button onClick={() => goodButtonHandler(good)}>Good</button>
       <button onClick={() => neutralButtonHandler(neutral)}>Neutral</button>
-      <button onClick={() => badButtonHandler(bad)}>Good</button>
-      < Content
+      <button onClick={() => badButtonHandler(bad)}>Bad</button>
+      <Content
       good={good}
       neutral={neutral}
       bad={bad}
+      all={total}
+      average={average / total}
+      positive={positive}
       />
     </div>
   );
