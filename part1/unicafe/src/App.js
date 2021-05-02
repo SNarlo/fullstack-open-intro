345284dc1c4e74af2c394b28ef6afe3856ea529b
Sebastian <sebastian.narloch82@gmail.com>
2021-05-02 13:09:50 +1000

    Completedd exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,13 +4,28 @@ const Content = (props) => {
   return (
     <div>
       <h2>Statistics</h2>
+      <Statistics 
+      good={props.good}
+      bad={props.bad}
+      neutral={props.neutral}
+      all={props.all}
+      average={props.average}
+      positive={props.positive}
+      />
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  return (
+    <>
       <p>Good: {props.good}</p>
       <p>Neutral: {props.neutral}</p>
       <p>Bad: {props.bad}</p>
       <p>All: {props.all}</p>
       <p>Average {props.average}</p>
       <p>Positive {props.positive}%</p>
-    </div>
+    </>
   )
 }
 
@@ -50,10 +65,10 @@ const App = () => {
       <button onClick={() => goodButtonHandler(good)}>Good</button>
       <button onClick={() => neutralButtonHandler(neutral)}>Neutral</button>
       <button onClick={() => badButtonHandler(bad)}>Bad</button>
-      <Content
+      <Content 
       good={good}
-      neutral={neutral}
       bad={bad}
+      neutral={neutral}
       all={total}
       average={average / total}
       positive={positive}
