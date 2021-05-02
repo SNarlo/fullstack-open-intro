3faa40440e2977273f202a194b785fe5437a71a8
Sebastian <sebastian.narloch82@gmail.com>
2021-05-02 13:35:50 +1000

    Completed exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,31 +1,49 @@
 import React, {useState} from 'react'
 
 const Content = (props) => {
+  if (props.stats) {
+    return (
+      <div>
+        <Statistics 
+        good={props.good}
+        bad={props.bad}
+        neutral={props.neutral}
+        all={props.all}
+        average={props.average}
+        positive={props.positive}
+        />
+      </div>
+    )
+  }
   return (
-    <div>
-      <h2>Statistics</h2>
-      <Statistics 
-      good={props.good}
-      bad={props.bad}
-      neutral={props.neutral}
-      all={props.all}
-      average={props.average}
-      positive={props.positive}
-      />
-    </div>
+    <div></div>
+  )
+}
+
+const NoFeedback = (props) => {
+  if (!props.stats) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <div></div>
   )
 }
 
 const Statistics = (props) => {
   return (
-    <>
+    <div className="stats">
       <p>Good: {props.good}</p>
       <p>Neutral: {props.neutral}</p>
       <p>Bad: {props.bad}</p>
       <p>All: {props.all}</p>
       <p>Average {props.average}</p>
       <p>Positive {props.positive}%</p>
-    </>
+    </div>
   )
 }
 
@@ -37,8 +55,10 @@ const App = () => {
   const [total, setTotal] = useState(0)
   const [average, setAverage] = useState(0)
   const [positive, setPositive] = useState(0)
+  const [stats, hasStats] = useState(false)
 
   const goodButtonHandler = (value) => {
+    hasStats(true)
     setGood(value += 1)
     setTotal(total + 1)
     setPositive((good + 1) / (total + 1) * 100)
@@ -46,12 +66,14 @@ const App = () => {
   }
   
   const neutralButtonHandler = (value) => {
+    hasStats(true)
     setNeutral(value += 1)
     setTotal(total + 1)
     setPositive((good / (total + 1)) * 100)
   }
   
   const badButtonHandler = (value) => {
+    hasStats(true)
     setBad(value += 1)
     setTotal(total + 1)
     setPositive((good / (total + 1)) * 100)
@@ -65,7 +87,9 @@ const App = () => {
       <button onClick={() => goodButtonHandler(good)}>Good</button>
       <button onClick={() => neutralButtonHandler(neutral)}>Neutral</button>
       <button onClick={() => badButtonHandler(bad)}>Bad</button>
+      <NoFeedback stats={stats}/>
       <Content 
+      stats={stats}
       good={good}
       bad={bad}
       neutral={neutral}
