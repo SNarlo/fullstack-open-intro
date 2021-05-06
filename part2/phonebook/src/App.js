7682fbec635fb465b6aa5d4de8c137d3a6453619
Sebastian <sebastian.narloch82@gmail.com>
2021-05-06 14:42:12 +1000

    Completed Exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,20 +4,45 @@ const Numbers = (props) => {
   return (
     <div>
       {props.persons.map(person => 
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>{person.name} {person.number}</p>
       )}
     </div>
   )
 }
 
+const Input = (props) => {
+  return (
+    <>
+      <br></br>
+      {props.name} <input onChange={props.change} value={props.val}/>
+      <br></br>
+    </>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <>
+      <br></br>
+      <button type='submit' onClick={props.click}>{props.name}</button>
+    </>
+  )
+}
+
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '0123' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
  
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const checkIfIncludedAlready = (newName) => {
@@ -35,15 +60,19 @@ const App = () => {
   const addNewPerson = (event) => {
     event.preventDefault()
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
+    
 
     if (checkIfIncludedAlready(newPerson.name)) {
       window.alert(`${newPerson.name} + " is already added`)
       setNewName('')
+      setNewNumber('')
     } else {
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -52,10 +81,11 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={handleNameChange} value={newName}/>
+          <Input name={'name: '} change={handleNameChange} val={newName}/>
+          <Input name={'number: '} change={handleNumberChange} val={newNumber}/>
         </div>
         <div>
-          <button type="submit" onClick={addNewPerson}>add</button>
+          <Button name='add' click={addNewPerson}/>
         </div>
       </form>
       <h2>Numbers</h2>
