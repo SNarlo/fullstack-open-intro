ff5d3f1fd0612cba997566ef6f87327a1739e9e2
Sebastian <sebastian.narloch82@gmail.com>
2021-05-14 19:22:05 +1000

    Started excercise 2.13, adding a button to show more info

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -13,8 +13,8 @@ const Output = (props) => {
   return (
     <div>
       {props.countries.map(country => 
-        <p key={country.name}>{country.name}</p>
-      )}
+        <p key={country.name}>{country.name} <button onClick={props.click}>Show More</button></p>
+      )} 
     </div>
   )
 }
@@ -29,6 +29,7 @@ const SingleCountry = (props) => {
       <h2>Languages</h2>
       {props.language.map(language => 
       <li key={language.name}>{language.name}</li>)}
+  
       <img src={props.flag} alt="country flag" height="100px" width="120px"></img>
     </div>
   )
@@ -37,7 +38,7 @@ const SingleCountry = (props) => {
 const App = () => {
   const [countries, setCountries] = useState([])
   const [filteredCountries, setFiltered] = useState([])
-  const [displayedContent, setDisplayedContent] = useState()
+  const [displayedComponent, setDisplayedComponent] = useState()
 
   useEffect(() => {
     axios
@@ -47,22 +48,24 @@ const App = () => {
     })
   }, [])
 
+  const 
+
   const filterCountries = (event) => {
     let tempArray = (countries.filter(country => country.name.toLowerCase().includes(event.target.value)))
     
     if (tempArray.length < 10) {
       setFiltered(tempArray)
-      setDisplayedContent(<Output countries={filteredCountries}/>)
+      setDisplayedComponent(<Output countries={filteredCountries}/>)
     } 
     
     if (tempArray.length >= 10) {
       let tooManySearches = [{name: 'Too many matches, specify search more'}]
       setFiltered(tooManySearches)
-      setDisplayedContent(<Output countries={filteredCountries}/>)
+      setDisplayedComponent(<Output countries={filteredCountries}/>)
     }
 
     if (tempArray.length === 1) {
-      setDisplayedContent(
+      setDisplayedComponent(
       <SingleCountry 
         country={tempArray[0].name} 
         capital={tempArray[0].capital} 
@@ -77,7 +80,7 @@ const App = () => {
    <div>
      <Input text={'Find countries: '} changed={filterCountries}/>
      <div>
-       {displayedContent}
+       {displayedComponent}
      </div>
    </div> 
   )
