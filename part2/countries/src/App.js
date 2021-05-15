b86c93b7e62106b203fd7a6bb54e1645388a2c61
Sebastian <sebastian.narloch82@gmail.com>
2021-05-15 11:35:41 +1000

    Working on exercise 2.14, need to find a way to access weather data

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect} from 'react'
+import useState from 'react-usestateref'
 import axios from 'axios'
 
 const Input = (props) => {
@@ -19,6 +20,17 @@ const Output = (props) => {
   )
 }
 
+const Weather = (props) => {
+  return (
+    <div>
+      <h2>Weather in {props.city}</h2>
+
+      <p><b>Temperature: </b>{props.temp}</p>
+      {/* <p><b>Wind: </b>{props.wind}</p> */}
+    </div>
+  )
+}
+
 const SingleCountry = (props) => {
   return (
     <div>
@@ -31,14 +43,18 @@ const SingleCountry = (props) => {
       <li key={language.name}>{language.name}</li>)}
   
       <img src={props.flag} alt="country flag" height="100px" width="120px"></img>
+
+      {props.weather}
     </div>
   )
 }
+
 
 const App = () => {
   const [countries, setCountries] = useState([])
   const [filteredCountries, setFiltered] = useState([])
   const [displayedComponent, setDisplayedComponent] = useState()
+  const [weather, setWeather] = useState([])
 
   useEffect(() => {
     axios
@@ -48,17 +64,31 @@ const App = () => {
     })
   }, [])
 
-  const expandCountry = (event) => {
+  const setWeatherForCapital = (capital) => {
+    const params = {
+      access_key : '0cbacbaf8ace377452f0ddd9fc785b14',
+      query : capital
+    }
 
-    let selectedCountry = countries.filter(country => country.name === event.target.id)  
+    axios
+    .get('http://api.weatherstack.com/current', {params})
+    .then(response => {
+      setWeather(response)
+    })
+  }  
+
+  const expandCountry = (event) => {
+    let selectedCountry = countries.filter(country => country.name === event.target.id) 
+    setWeatherForCapital(selectedCountry[0].capital) 
+    
     setDisplayedComponent(<SingleCountry 
       country={selectedCountry[0].name} 
       capital={selectedCountry[0].capital} 
       population={selectedCountry[0].population} 
       language={selectedCountry[0].languages}
-      flag={selectedCountry[0].flag} />)
+      flag={selectedCountry[0].flag} 
+      weather={<Weather city={selectedCountry[0].capital} temp={weather.temperature}/>}/>)
   }
-
 
   const filterCountries = (event) => {
     let tempArray = (countries.filter(country => country.name.toLowerCase().includes(event.target.value)))
@@ -80,6 +110,7 @@ const App = () => {
         population={tempArray[0].population} 
         language={tempArray[0].languages}
         flag={tempArray[0].flag}
+        weather={<Weather city={tempArray[0].capital}/>}
       />)
     }
   }
