0c4d7ba86005694580602443b52aab3a2ca9c8c5
Sebastian <sebastian.narloch82@gmail.com>
2021-05-16 10:57:28 +1000

    Added axios services module and code for functions

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,3 +1,20 @@
 import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/persons'
+
+const getAllPersons = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+export default {getAllPersons, create, update}
